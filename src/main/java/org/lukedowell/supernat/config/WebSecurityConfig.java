782a2cc1322f44f1b6840bdc5f2dc00fec7aa9dd
Luke Dowell <ldowell@dhcp-16-5-132.bravo>
2015-11-19 16:28:34 -0600

    Fixes configuration annotation on websecurityconfig

--- src/main/java/org/lukedowell/supernat/config/WebSecurityConfig.java
+++ src/main/java/org/lukedowell/supernat/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.lukedowell.supernat.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +11,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * Created by ldowell on 11/19/15.
  */
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin().and()
+                .httpBasic();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
