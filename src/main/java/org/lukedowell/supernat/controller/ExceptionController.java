2062d442450b039e2cd5cc8698077536d16b231c
Luke Dowell <lukedowell@gmail.com>
2015-11-25 10:31:37 -0600

    ExceptionController skeleton

---
+++ src/main/java/org/lukedowell/supernat/controller/ExceptionController.java
@@ -0,0 +1,12 @@
+package org.lukedowell.supernat.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ldowell on 11/24/15.
+ */
+@Controller
+public class ExceptionController {
+
+
+}
