ef1c4d955f50647f884833f8a93ec389ff4a5adb
Luke Dowell <lukedowell@gmail.com>
2015-12-03 12:35:38 -0600

    rename view resolver config, reports work now wooo

--- src/main/java/org/lukedowell/supernat/config/JasperConfig.java
+++ src/main/java/org/lukedowell/supernat/config/JasperConfig.java
@@ -1,5 +1,6 @@
 package org.lukedowell.supernat.config;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,7 @@ import java.sql.DriverManager;
  * Created by ldowell on 12/3/15.
  */
 @Configuration
-public class ViewResolverConfig {
+public class JasperConfig {
 
     @Bean
     public JasperReportsViewResolver getJasperReportsViewResolver() {
@@ -22,17 +23,17 @@ public class ViewResolverConfig {
         resolver.setSuffix(".jasper");
         resolver.setViewNames("rpt_*");
         resolver.setViewClass(JasperReportsMultiFormatView.class);
+        resolver.setJdbcDataSource(getMySqlDataSource());
         resolver.setOrder(1);
         return resolver;
     }
 
-//    @Bean
-//    public FreeMarkerViewResolver viewResolver() {
-//        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
-//        resolver.setCache(true);
-//        resolver.setPrefix("classpath:/templates/");
-//        resolver.setSuffix(".ftl");
-//        resolver.setOrder(0);
-//        return resolver;
-//    }
+    @Bean
+    MysqlDataSource getMySqlDataSource() {
+        MysqlDataSource source = new MysqlDataSource();
+        source.setURL("jdbc:mysql://localhost:3306/supernat");
+        source.setUser("root");
+        source.setPassword("password");
+        return source;
+    }
 }
